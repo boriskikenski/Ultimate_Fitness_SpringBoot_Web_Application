732c61add2433e7e0581a5b154909f48e28e5471
Boris Kikenski <bkikenski@gmail.com>
2023-06-30 13:43:48 +0200

    init commit

---
+++ src/main/java/com/bkikenski/ultimatefitness/UltimateFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.bkikenski.ultimatefitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateFitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateFitnessApplication.class, args);
+    }
+
+}
