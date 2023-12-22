1819ad7e205073084cf601ca24745d50f2530b22
Boris Kikenski <bkikenski@gmail.com>
2023-12-22 10:44:59 +0100

    added constructor to enums, updated printing in register forms

--- src/main/java/com/bkikenski/ultimatefitness/model/enumerations/FitnessPlans.java
+++ src/main/java/com/bkikenski/ultimatefitness/model/enumerations/FitnessPlans.java
@@ -1,12 +1,23 @@
 package com.bkikenski.ultimatefitness.model.enumerations;
 
 public enum FitnessPlans {
-    BODYBUILDING,
-    POWERLIFTING,
-    CROSSFIT,
-    OLYMPIC,
-    CARDIO,
-    BASIC_3_DAYS,
-    BASIC_4_DAYS,
-    BASIC_5_DAYS
+    BODYBUILDING("Bodybuilding"),
+    POWERLIFTING("Powerlifting"),
+    CROSSFIT("Crossfit"),
+    OLYMPIC("Olympic"),
+    CARDIO("Cardio"),
+    BASIC_3_DAYS("Basic 3 days plan"),
+    BASIC_4_DAYS("Basic 4 days plan"),
+    BASIC_5_DAYS("Basic 5 days plan")
+    ;
+
+    private final String fitnessPlan;
+
+    FitnessPlans(String fitnessPlan) {
+        this.fitnessPlan = fitnessPlan;
+    }
+
+    public String getFitnessPlan() {
+        return fitnessPlan;
+    }
 }
--- src/main/java/com/bkikenski/ultimatefitness/model/enumerations/Goals.java
+++ src/main/java/com/bkikenski/ultimatefitness/model/enumerations/Goals.java
@@ -1,10 +1,21 @@
 package com.bkikenski.ultimatefitness.model.enumerations;
 
 public enum Goals {
-    LOSE_WEIGHT,
-    GAIN_WEIGHT,
-    FITNESS,
-    POWER,
-    STAMINA,
-    HEALTH_AND_RECREATION
+    LOSE_WEIGHT("My goal is to lose weight"),
+    GAIN_WEIGHT("My goal is to gain weight"),
+    FITNESS("My goal is to have better overall fitness shape"),
+    POWER("My goal is to have more power, be stronger"),
+    STAMINA("My goal is to have more stamina"),
+    HEALTH_AND_RECREATION("My goal is to improve health and recreate")
+    ;
+
+    private final String goal;
+
+    Goals(String goal) {
+        this.goal = goal;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
 }
