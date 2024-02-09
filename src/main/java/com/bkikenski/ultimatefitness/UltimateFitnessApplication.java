6cb0b3ae9ee1522119884203af87f3c342ac22e1
Boris Kikenski <bkikenski@gmail.com>
2024-02-09 10:24:23 +0100

    implemented scheduler for generating training and diet plan for every user weekly

--- src/main/java/com/bkikenski/ultimatefitness/UltimateFitnessApplication.java
+++ src/main/java/com/bkikenski/ultimatefitness/UltimateFitnessApplication.java
@@ -2,8 +2,10 @@ package com.bkikenski.ultimatefitness;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UltimateFitnessApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/bkikenski/ultimatefitness/config/PlanGenerationScheduler.java
@@ -0,0 +1,37 @@
+package com.bkikenski.ultimatefitness.config;
+
+import com.bkikenski.ultimatefitness.model.User;
+import com.bkikenski.ultimatefitness.repository.UserRepository;
+import com.bkikenski.ultimatefitness.service.DietPlanService;
+import com.bkikenski.ultimatefitness.service.TrainingPlanService;
+import com.bkikenski.ultimatefitness.service.UserService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PlanGenerationScheduler {
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final TrainingPlanService trainingPlanService;
+    private final DietPlanService dietPlanService;
+
+    public PlanGenerationScheduler(UserRepository userRepository, UserService userService,
+                                   TrainingPlanService trainingPlanService, DietPlanService dietPlanService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+        this.trainingPlanService = trainingPlanService;
+        this.dietPlanService = dietPlanService;
+    }
+
+    @Scheduled(cron = "0 0 0 * * SUN")
+    public void generateTrainingPlan() {
+        List<User> allUsers = userRepository.findAll();
+        allUsers.forEach(user -> {
+            userService.setUserLevel(user.getUserId());
+            trainingPlanService.generateTrainingPlan(user.getUserId());
+            dietPlanService.generateDietPlan(user.getUserId());
+        });
+    }
+}
